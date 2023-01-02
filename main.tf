98dc746e46cfda6f026cf593bef29f5abc076bb5
Mathew <mathew.wong@waymarkcare.com>
2023-01-02 14:42:11 -0800

    Plugging security roles.

--- main.tf
+++ main.tf
@@ -111,14 +111,25 @@ resource "aws_volume_attachment" "jupyter" {
   force_detach = true
 }
 
-resource "aws_s3_bucket" "manifest_bucket" {
-  bucket = "manifest-bucket-${uuid()}"
+resource "aws_kms_key" "manifest_key" {
+  description             = "This key is used to encrypt the manifest_bucket"
+  deletion_window_in_days = 10
 }
 
-resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.manifest_bucket.id
-  key    = "manifest.json"
-  source = "./manifest.json"
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.manifest_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.manifest_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "manifest_bucket" {
+  bucket = "manifest-bucket-${uuid()}"
+  restrict_public_buckets = true
 }
 
 data "aws_iam_policy_document" "S3_automation_move_objects" {
